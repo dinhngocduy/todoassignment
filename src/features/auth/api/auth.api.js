1fcc48177ed66ed0328d0d366d9ae0c4498b01c0
dinhngocduy <catdere1234@gmail.com>
2022-02-15 15:10:14 +0700

    todo

---
+++ src/features/auth/api/auth.api.js
@@ -0,0 +1,15 @@
+import api from "api/api";
+
+export const loginApi = ({ username, password }) => {
+  return api.post("/auth/login", {
+    username,
+    password,
+  });
+};
+
+export const registerApi = ({ username, password }) => {
+  return api.post("/auth/register", {
+    username,
+    password,
+  });
+};
